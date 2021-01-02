155e0430ebf502f9bd4d0cd38a4dea3566c9ca9d
Steven Woo <swoo9875@gmail.com>
2021-01-02 12:42:01 -0500

    added variables and prompts for the password

--- Develop/script.js
+++ Develop/script.js
@@ -12,3 +12,20 @@ function writePassword() {
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
+
+function generatePassword(){
+  //created variable names for uppercase, lowercase, special characters, and numbers as arrays
+    var upperCase = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
+    var lowerCase = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
+    var specialChar = ["!", "#", "$", "%", "&", "(", ")", "*", "+", "-", "/", ":", ";", "<", ">", "=", "?", "@", "^", "_", "~"];
+    var num = ["1", "2", "3", "4", "5", "6", "7", "8", "9", "0"];
+    var passwordLength = false;
+
+    while(passwordLength < 8 || passwordLength > 129 || passwordLength == ""){
+      var passwordLength = prompt("Choose a password with a length between 8 and 128");
+    }
+    var pickUpperCase = confirm("Do you want uppercase in your password")
+    var pickLowerCase = confirm("Do you want lowercase in your password")
+    var pickSpecialChar = confirm("Do you want special character in your password")
+    var pickNum = confirm("Do you want numebers in your password")
+  }
